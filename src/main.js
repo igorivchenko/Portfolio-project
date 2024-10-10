b26eeaa88b2277c9f7d6af6f803903d5bc3c48e9
Igor Ivchenko <iv4enkoigor@gmail.com>
2024-10-10 20:29:42 +0300

    update project

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/dark-theme';
+import './js/header';
+import './js/my-projects';
+import './js/FAQ-accordeon';
+import './js/reviews-api';
+import './js/reviews-swiper';
+import './js/work-together-api';
