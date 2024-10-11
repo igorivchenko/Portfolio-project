68e870ce59e9b3caf498b0da1fb6e9947cc6263c
Roman Hudz <hudzrom@gmail.com>
2024-10-11 20:35:35 +0300

    Секція FAQ (HTML, JS готові)
    Попередньо виконано розмітку та JS з імпортом бібліотек (в подальшому можливі правки) . Починаю додавати стилі.

---
+++ src/js/FAQ-accordeon.js
@@ -0,0 +1,6 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Accordion('.accordion-container');
+});
