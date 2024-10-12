2a17912cf6a46f98784a17e5f98946a799511506
Roman Hudz <hudzrom@gmail.com>
2024-10-12 21:37:19 +0300

    Загальна структура готова, потрібно допрацювати стилі

--- src/js/FAQ-accordeon.js
+++ src/js/FAQ-accordeon.js
@@ -1,6 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-document.addEventListener('DOMContentLoaded', function () {
-  new Accordion('.accordion-container');
+new Accordion('.accordion-container', {
+  openOnInit: [0],
 });
