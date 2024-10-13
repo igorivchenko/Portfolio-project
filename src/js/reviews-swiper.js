99c14d6c6c6ab3e1b8033559df83534db4ca2c41
nazarufakit <onazik08072005@gmail.com>
2024-10-13 13:01:25 +0300

    swiper delete comment

--- src/js/reviews-swiper.js
+++ src/js/reviews-swiper.js
@@ -23,34 +23,4 @@ const swiper = new Swiper('.swiper', {
 
   slidesPerView: 1,
   spaceBetween: 32,
-
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 32,
-  //     },
-  //     768: {
-  //       slidesPerView: 3,
-  //       spaceBetween: 32,
-  //     },
-  //     1280: {
-  //       slidesPerView: 4,
-  //       spaceBetween: 32,
-  //     },
-  //   },
-
-  //   breakpoints: {
-  //     640: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 20,
-  //     },
-  //     768: {
-  //       slidesPerView: 4,
-  //       spaceBetween: 40,
-  //     },
-  //     1024: {
-  //       slidesPerView: 5,
-  //       spaceBetween: 50,
-  //     },
-  //   },
 });
