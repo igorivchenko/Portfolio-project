c4f64a2e1f0d29b9ebe305c7e47f3c39827b673d
nazarufakit <onazik08072005@gmail.com>
2024-10-13 16:20:07 +0300

    all done

--- src/js/reviews-markup.js
+++ src/js/reviews-markup.js
@@ -4,7 +4,16 @@ const buttonWrapper = document.querySelector('.button-wrapper');
 
 export function createMarkup(data) {
   const markup = data
-    .map(({ author, avatar_url, review }) => {
+    .map(({ author, avatar_url, review, _id }) => {
+      if (_id === 5) {
+        return `<li class="swiper-slide">
+					<p class="reviews-text">${review.slice(0, -31)}</p>
+					<div class="card-wrapper">
+						<img class="reviews-icon" src="${avatar_url}" alt="${author}">
+						<h2 class="reviews-author">${author}</h2>
+					</div>
+				</li>`;
+      }
       return `<li class="swiper-slide">
 					<p class="reviews-text">${review}</p>
 					<div class="card-wrapper">
