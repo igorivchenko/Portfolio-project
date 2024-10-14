b9ecd40580211cac1c23393a84948f007f145fc5
Borodkina Olena <elena.kaminnik@gmail.com>
2024-10-14 14:03:35 +0300

    header

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -4,24 +4,24 @@ let toggled = false;
 
 button.addEventListener('click', () => {
   toggled = !toggled;
-  
+
   const root = document.documentElement;
-  
+
   if (toggled) {
     root.style.setProperty('--white', '#494949');
     root.style.setProperty('--light-grey', '#2A2D32');
     root.style.setProperty('--black', '#F0F0F0');
     button.classList.add('right');
-     
-  
-}else{
+
+
+  } else {
     button.classList.remove('right');
-    
+
     root.style.setProperty('--white', '#F0F0F0');
     root.style.setProperty('--light-grey', '#e4e5e6');
     root.style.setProperty('--black', '#2A2D32');
-   
-     
+
+
   }
 });
 
