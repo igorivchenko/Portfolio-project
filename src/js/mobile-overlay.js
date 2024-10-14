f74a311f6ecd4a9909260b7638b117a71718196f
Igor Ivchenko <iv4enkoigor@gmail.com>
2024-10-14 22:00:13 +0300

    fix header

--- src/js/mobile-overlay.js
+++ src/js/mobile-overlay.js
@@ -1,23 +1,23 @@
 const refs = {
   openModalBtn: document.querySelectorAll('.js-open-menu'),
   closeModalBtn: document.querySelector('.js-close-menu'),
-  modal: document.querySelector('.js-menu-container')
-
-
+  modal: document.querySelector('.js-menu-container'),
 };
-refs.openModalBtn.forEach((element) => {
-  element.addEventListener("click", toggleModal);
-});
-refs.closeModalBtn.addEventListener("click", toggleModal);
 
-refs.modal.addEventListener("click", removeModal);
+refs.openModalBtn.forEach(element => {
+  element.addEventListener('click', toggleModal);
+});
+
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+refs.modal.addEventListener('click', removeModal);
 function removeModal(e) {
   if (e.target === e.currentTarget) {
-    refs.modal.classList.add("is-hidden");
+    refs.modal.classList.add('is-active');
   }
 }
 
 function toggleModal() {
-  refs.modal.classList.toggle("is-hidden");
-  document.body.classList.toggle("no-scroll");
-};
+  refs.modal.classList.toggle('is-active');
+  document.body.classList.toggle('no-scroll');
+}
